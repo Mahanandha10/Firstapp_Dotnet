8fca33e28d703e28ff407154d7a02028d099430d
Mahanandha10 <mahanandhan2002@gmail.com>
2024-11-07 20:03:22 +0530

    password commit

--- Program.cs
+++ Program.cs
@@ -1,7 +1,16 @@
 ﻿// First program in dotnet
-Console.WriteLine("What is your name");
-var name= Console.ReadLine();
-var currentDate=DateTime.Now;
-Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d}, at {currentDate:t}");
-Console.Write($"{Environment.NewLine}press any to exit...");
-Console.ReadKey(true);
+
+
+// Console.WriteLine("What is your name");
+// var name= Console.ReadLine();
+// var currentDate=DateTime.Now;
+// Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d}, at {currentDate:t}");
+// Console.Write($"{Environment.NewLine}press any to exit...");
+// Console.ReadKey(true);
+
+
+using PasswordGenerator;
+
+var pwd = new Password(includeLowercase: true, includeUppercase: true, includeNumeric: true, includeSpecial: false, passwordLength: 6);
+var password = pwd.Next();
+Console.WriteLine(password);
--- obj/Debug/net8.0/myfristapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myfristapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myfristapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a931c4a350cb169d73b65467e503a99fbf8fc9fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8236aba6a8ff3e0191c03f195791f1b36dd49eb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("myfristapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myfristapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
