8236aba6a8ff3e0191c03f195791f1b36dd49eb3
Mahanandha10 <mahanandhan2002@gmail.com>
2024-11-07 11:06:49 +0530

    second commit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,6 @@
 Console.WriteLine("What is your name");
 var name= Console.ReadLine();
 var currentDate=DateTime.Now;
-Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d}");
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d}, at {currentDate:t}");
 Console.Write($"{Environment.NewLine}press any to exit...");
 Console.ReadKey(true);
