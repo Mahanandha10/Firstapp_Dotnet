a931c4a350cb169d73b65467e503a99fbf8fc9fe
Mahanandha10 <mahanandhan2002@gmail.com>
2024-11-07 09:22:15 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// First program in dotnet
+Console.WriteLine("What is your name");
+var name= Console.ReadLine();
+var currentDate=DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d}");
+Console.Write($"{Environment.NewLine}press any to exit...");
+Console.ReadKey(true);
